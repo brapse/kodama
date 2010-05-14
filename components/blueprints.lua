5ea5c2b730a190616141b2f0480435d0a96363b1
Henk Boom <henk@invincible-spell.(none)>
2010-05-14 16:14:08 -0400

    changed the player size

--- components/blueprints.lua
+++ components/blueprints.lua
@@ -2,7 +2,7 @@ local v2 = require 'dokidoki.v2'
 local game = require 'dokidoki.game'
 
 player = game.make_blueprint('player',
-  {'transform', pos=v2(100, 100), scale_x=8, scale_y=8},
+  {'transform', pos=v2(100, 100), scale_x=24, scale_y=32},
   {'sprite'},
   {'player_movement'})
 
