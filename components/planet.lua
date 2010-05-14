a6a4d1aab3b59f43fdc3035bd3a9e98e10993b85
Henk Boom <henk@invincible-spell.(none)>
2010-05-14 16:15:32 -0400

    planet draws

--- components/planet.lua
+++ components/planet.lua
@@ -1,1 +1,17 @@
-radius = 10
+local gl = require 'gl'
+local v2 = require 'dokidoki.v2'
+
+radius = 100
+
+local SEGMENTS = 64
+
+game.actors.new_generic('planet_component', function()
+  function draw()
+    gl.glBegin(gl.GL_POLYGON)
+    for i = 1, SEGMENTS do
+      local point = v2.unit(i/SEGMENTS * 2*math.pi) * radius
+      gl.glVertex2d(point.x, point.y)
+    end
+    gl.glEnd();
+  end
+end)
