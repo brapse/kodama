1233d55bd034728ebc877e3bbdafdc8717fa3027
Sean Braithwaite <brapse@gmail.com>
2010-05-21 17:23:23 -0400

    Add sprites
        + player placeholder
        + plant placehodler
        + center player sprite

--- components/blueprints.lua
+++ components/blueprints.lua
@@ -1,14 +1,13 @@
 local v2 = require 'dokidoki.v2'
-local game = require 'dokidoki.game'
 
 player = game.make_blueprint('player',
-  {'transform', pos=v2(100, 100), scale_x=24, scale_y=32},
-  {'sprite'},
+  {'transform', pos=v2(100, 100) },
+  {'sprite', image=game.resources.player_sprite},
   {'collider'},
   {'player_movement'})
 
 plant = game.make_blueprint('plant',
-  {'transform', scale_x=8, scale_y=8},
-  {'sprite'},
+  {'transform'},
+  {'sprite', image=game.resources.plant_sprite},
   {'collider'})
 
---
+++ components/resources.lua
@@ -0,0 +1,4 @@
+local graphics = require 'dokidoki.graphics'
+
+player_sprite = graphics.sprite_from_image('sprites/player.png', nil, {12, 0})
+plant_sprite  = graphics.sprite_from_image('sprites/plant.png', nil, {4,0})
