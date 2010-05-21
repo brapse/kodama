e1cb75cd94d1e4c57e637695177c9a2b7cf01c10
Henk Boom <henk@henk.ca>
2010-05-21 16:30:24 -0400

    player speed increase

--- scripts/player_movement.lua
+++ scripts/player_movement.lua
@@ -4,15 +4,17 @@ local left_key = string.byte('A')
 local right_key = string.byte('D')
 local plant_key = string.byte(' ')
 
+local speed = 2
+
 function update()
   local right = self.transform.facing
 
   if game.keyboard.key_held(left_key) then
-    self.transform.pos = self.transform.pos - right
+    self.transform.pos = self.transform.pos - right * speed
   end
 
   if game.keyboard.key_held(right_key) then
-    self.transform.pos = self.transform.pos + right
+    self.transform.pos = self.transform.pos + right * speed
   end
 
   if game.keyboard.key_pressed(plant_key) then
