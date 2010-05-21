add1fdb0c7b683cd8ac8bdb5706e983af5c24b72
Sean Braithwaite <brapse@gmail.com>
2010-05-21 18:35:58 -0400

    Player physics
        + players have a velocity vector
        + left and right movement now add and subtract
          velocity vectors orthogonal to the planet
        + gravitational force moves player towards the center of the planet
        + contstraint keeps objects from going into the planet
        + 'w' key jumps

--- scripts/collider.lua
+++ scripts/collider.lua
@@ -1,9 +1,36 @@
 local v2 = require 'dokidoki.v2'
 
+velocity = v2.zero
+gravity  = -0.4
+local decay = 0.99
+
+function update()
+  -- apply gravity
+  apply_impulse(gravity * v2.norm(self.transform.pos))
+  -- decay velocity
+  velocity = velocity * decay
+  self.transform.pos = self.transform.pos + velocity
+
+end
+
+--handle player movement
 function update_correction()
   if self.transform.pos ~= v2.zero then
-    local up = v2.norm(self.transform.pos)
-    self.transform.pos = up * game.planet.radius
+    local distance = v2.mag(self.transform.pos)
+    local up = self.transform.pos / distance
+    local is_inside = distance < game.planet.radius
+
     self.transform.facing = -v2.rotate90(up)
+
+    -- keep player from being inside the planet
+    if is_inside then
+      velocity = velocity - v2.project(velocity, up)
+      self.transform.pos = up * game.planet.radius
+    end
   end
+
+end
+
+function apply_impulse(impulse)
+  velocity = velocity + impulse
 end
